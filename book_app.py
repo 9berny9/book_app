99c343d09b10a88a2f28d8f8273a4c4a693e06e9
9berny9 <bernatik.michal@gmail.com>
2022-04-11 00:31:53 +0200

    confirm your select

--- book_app.py
+++ book_app.py
@@ -23,27 +23,29 @@ Find friends for your book!
 """)
 
 user_name = st.text_input("Enter your name:")
-st.write(f'You selected: {user_name}')
-genres = st.multiselect("Select your favorite genres:", genres)
-book_author = st.text_input("What is your favorite author?")
+if user_name:
+    st.write(f'You selected: {user_name}')
+    genres = st.multiselect("Select your favorite genres:", genres)
+    if genres:
+        book_author = st.text_input("What is your favorite author?")
 
-if book_author:
-    st.write(f'You selected: {book_author}')
-    books_finding = book_rec.books[book_rec.books['Book-Author'].str.contains(book_author, case=False, na=True)]
+        if book_author:
+            st.write(f'You selected: {book_author}')
+            books_finding = book_rec.books[book_rec.books['Book-Author'].str.contains(book_author, case=False, na=True)]
 
-#book_author = st.selectbox('Select your favorite book author', books_finding["Book-Author"])
-#st.write(f'You selected: {book_author}')
+        #book_author = st.selectbox('Select your favorite book author', books_finding["Book-Author"])
+        #st.write(f'You selected: {book_author}')
 
 
-#book_title = book_rec.books.loc[book_rec.books['Book-Author'] == book_author]['Book-Title']
-    book_name = st.selectbox(f'Select your favorite book from {book_author}', books_finding['Book-Title'])
-    st.write(f'You selected: {book_name}')
+        #book_title = book_rec.books.loc[book_rec.books['Book-Author'] == book_author]['Book-Title']
+            book_name = st.selectbox(f'Select your favorite book from {book_author}', books_finding['Book-Title'])
+            st.write(f'You selected: {book_name}')
 
-    if st.button("Get recommendations"):
-        result = book_rec.main(book_name.lower(), book_author)
-        if len(result[0]) == 0:
-            st.write("Sorry I don't have enough data for this book")
-        else:
-            st.write(result[0])
+            if st.button("Get recommendations"):
+                result = book_rec.main(book_name.lower(), book_author)
+                if len(result[0]) == 0:
+                    st.write("Sorry I don't have enough data for this book")
+                else:
+                    st.write(result[0])
 
 
