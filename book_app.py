b4117cd168703935aa5dd08785044968176b7643
9berny9 <bernatik.michal@gmail.com>
2022-04-15 01:18:44 +0200

    search bar is ready

--- book_app.py
+++ book_app.py
@@ -15,19 +15,16 @@ if user_name:
     st.write(f'You selected: {user_name}')
     genres = st.multiselect("Select your favorite genres:", fnc.genres)
     if genres:
-        book_author = st.selectbox(f"Select your favorite author:", fnc.dataset_authors)
+        book_author = st.selectbox(f"Select your favorite author:", fnc.author_list)
         book_author_find = book_author.lower()
 
         if book_author:
             st.write(f'You selected: {book_author}')
-            books_finding = fnc.dataset_base[fnc.dataset_base['Book-Author'].str.contains(book_author_find, case=False, na=True, flags=re.IGNORECASE, regex=False)]
-            books_finding = fnc.unique_list(books_finding, 'Book-Title')
+            books_finding = fnc.books_relevant['Book-Title'][fnc.books_relevant['Book-Author'].str.contains(book_author_find, case=False, na=True, flags=re.IGNORECASE, regex=False)]
+            books_finding = books_finding.to_frame()
+            books_list = fnc.get_data(books_finding, 'Book-Title')
 
-
-            # st.write(f'You selected: {book_author}')
-
-            # book_title = book_rec.books.loc[book_rec.books['Book-Author'] == book_author]['Book-Title']
-            book_name = st.selectbox(f'Select your favorite book from {book_author}', books_finding)
+            book_name = st.selectbox(f'Select your favorite book from {book_author}', books_list)
             st.write(f'You selected: {book_name}')
             book_name = book_name.lower()
             more_books = [book_name]
--- functions.py
+++ functions.py
@@ -1,26 +1,33 @@
 import book_rec as bc
+import pandas as pd
 
 
-def unique_list(data, column):
-    relevant_data = data.groupby([column]).count()
-    relevant_data = relevant_data.sort_values("User-ID").reset_index()
-    relevant_data = relevant_data[column][relevant_data['User-ID'] > 20]
+def unique_list(data):
+    relevant_data = data.groupby('Book-Author').count()
+    relevant_data = relevant_data.sort_values("Book-Author").reset_index()
     relevant_data = relevant_data.to_list()
     relevant_data.insert(0, '')
     return relevant_data
 
+def relevant_books(data):
+    ratings_relevant = data.groupby(['ISBN']).count()
+    ratings_relevant = ratings_relevant.reset_index()
+    ratings_relevant = ratings_relevant['ISBN'][ratings_relevant['User-ID'] > 70]
+    ratings_relevant = data[data['ISBN'].isin(ratings_relevant)]
+    return ratings_relevant
 
-relevant_data = ratings.groupby(['ISBN']).count()
-relevant_data = relevant_data.reset_index()
-relevant_data = relevant_data['ISBN'][relevant_data['User-ID'] > 5]
-# ted mam knizky, ktere byly hodnoceny aspon 5x - > spojim je s knizkami vsemi
-final = pd.merge(relevant_data, books, on=['ISBN'], how='inner')
-
-
+def get_data(data, column):
+    relevant_data = data.groupby([column]).count()
+    relevant_data = relevant_data.sort_values(by=column).reset_index()
+    relevant_data = relevant_data[column].to_list()
+    relevant_data.insert(0, '')
+    return relevant_data
 
 dataset_base = bc.dataset_merge()
 dataset_lowercase = bc.lowercase(dataset_base)
-dataset_authors = unique_list(dataset_base, 'Book-Author')
+books_relevant = relevant_books(dataset_base)
+author_list = get_data(books_relevant, 'Book-Author')
+
 
 genres = ["Art", "Business", "Chick-Lit", "Children's", "Christian", "Classics", "Comendy",
           "Comics", "Contemporary", "Cookbooks", "Crime", "Ebooks", "Fantasy", "Fiction", "Graphic Novels",
