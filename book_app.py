8f8d45f118af6b82360b841f7c67e20a2b5a4da8
9berny9 <bernatik.michal@gmail.com>
2022-04-18 17:58:25 +0200

    sorting functions

--- book_app.py
+++ book_app.py
@@ -19,59 +19,6 @@ def header(data_b, data_r):
     col2.metric(label="Number of Books", value=f"""{len(data_b['ISBN'])}""")
     col3.metric(label="Number of Ratings", value=f"""{len(data_r['Book-Rating'])}""")
 
-def main(books, ratings):
-    with open('style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-    st.image('book_logo.png', width=300)
-
-    col1, col2, col3 = st.columns(3)
-    col1.metric(label="Number of Users", value=f"""{len(ratings.groupby('User-ID'))}""")
-    col2.metric(label="Number of Books", value=f"""{len(books['ISBN'])}""")
-    col3.metric(label="Number of Ratings", value=f"""{len(ratings['User-ID'])}""")
-
-    user_name = st.text_input("Enter your name:")
-
-    #language_condition = br.languages[language]
-
-
-    if user_name:
-        select_genres = st.multiselect("Select your favorite genres:", br.genres)
-        if select_genres:
-            publisher_language = st.selectbox("Select language for publishers:", c.publisher_languages().keys())
-            if publisher_language:
-                book_author = st.text_input("Author's name:")
-                #book_author = st.selectbox(f"Select your favorite author:", c.get_language(books, publisher_language)['Book-Author'])
-                if book_author:
-                    books_finding = br.books_relevant['Book-Title'][
-                        br.books_relevant['Book-Author'].str.contains(book_author.lower(), case=False, na=True,
-                                                                      flags=re.IGNORECASE, regex=False)]
-                    books_list = br.get_data(books_finding.to_frame(), 'Book-Title')
-
-                    book_name = st.selectbox(f'Select your favorite book from {book_author}', books_list)
-                    check1, check2, check3 = st.columns(3)
-                    description_box = check1.checkbox("Book description", value=False)
-                    best_box = check2.checkbox("Best recommendations", value=False)
-                    worst_box = check3.checkbox("Worst recommendations", value=False)
-                    result = br.main(br.dataset_lowercase, book_name.lower(), book_author.lower(),
-                                     [book_name.lower()])
-
-                    if description_box:
-                        st.markdown('#### Book description:')
-                        book_description(book_name, book_author)
-
-                    if best_box:
-                        st.markdown('#### Best recommendations:')
-                        book_list_corr = result[0][0]['book'].to_list()
-                        recommendation(book_list_corr)
-
-                    if worst_box:
-                        st.markdown('#### Worst recommendations:')
-                        book_list_corr = result[1][0]['book'].to_list()
-                        recommendation(book_list_corr)
-
-
-
 
 def book_rec_img(book_list_corr, column, column_number, number):
     book_corr_data = br.get_dataset_for_corr(br.dataset_base, book_list_corr[column_number])
@@ -82,17 +29,6 @@ def book_rec_img(book_list_corr, column, column_number, number):
     column[number].image(book_cover, width=170,
                   caption=f'{book_corr_name} by {book_corr_author}')
 
-
-def books_short_desc(book_name, book_author):
-    url_img = br.get_book_img(br.dataset_base, book_name)
-    book_cover = Image.open(requests.get(url_img, stream=True).raw)
-    col1, col2, col3 = st.columns(3)
-    col1.image(book_cover, width=170)
-    col2.write(f"<b>{book_name}</b>", unsafe_allow_html=True)
-    col2.write(f"<b>{book_author}</b>", unsafe_allow_html=True)
-    col2.write(f"<b>{round(br.get_book_rating(br.dataset_base, book_name), 1)}/10</b>", unsafe_allow_html=True)
-    soup_for_book = ds.get_soup_book(book_name)
-    col2.write(ds.get_description(soup_for_book))
 
 def recommendation(book_list_corr):
     idx = 0
@@ -161,6 +97,8 @@ def merge_books(ratings_base, books_base):
 @st.cache(hash_funcs={StringIO: StringIO.getvalue}, suppress_st_warning=True)
 def data_lower(data):
     return data.apply(lambda x: x.str.lower() if (x.dtype == 'object') else x)
+
+
 
 
 if __name__ == "__main__":
